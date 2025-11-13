6b325b6b3ba0e9bd9d75f3d277c13e272696e395
Your Name <karpaty.tk@gmail.com>
2025-11-13 12:45:52 +0200

    Оновлення сайту: зміни локально та на GitHub

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 // Placeholder for site scripts
 console.log('Strahova site loaded');
+const menuToggle = document.getElementById('menu-toggle');
+const sideMenu = document.getElementById('side-menu');
+
+menuToggle.addEventListener('click', () => {
+  sideMenu.classList.toggle('show');
+});
